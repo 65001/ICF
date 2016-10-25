95ab3aa3f350e4817b9ddaf00283e63e8d660276
65001 <asathiabalan@gmail.com>
2016-10-25 18:36:01 -0400

    Todate

---
+++ Documents/Contents Table.sql
@@ -0,0 +1,73 @@
+Drop Table Contents;
+CREATE TABLE Contents(ID TEXT,Title TEXT,Author TEXT,Page_Number INTEGER,Copyright TEXT,Language TEXT);INSERT INTO Contents VALUES ('1','Abba Father','Dave Bilbrogh','1','1977 Thankyou Music','EN');
+INSERT INTO Contents VALUES ('2','Bind us together','Bob Gillman','1','1977 Thankyou Music','EN');
+INSERT INTO Contents VALUES ('3','Freely , Freely (God forgave my sin in Jesus Name)','Carol Owens','1','1972 Bud John Songs, Inc.','EN');
+INSERT INTO Contents VALUES ('4','Rejoice in the Lord','?','2',' ','EN');
+INSERT INTO Contents VALUES ('5','Spirit of the Living God','Daniel Iverson (?)','2',' ','EN');
+INSERT INTO Contents VALUES ('6','His name is wonderful','Audrey Mieir','2','1959 Audrey Mieir. Renewed 1987 Manna Music, Inc.','EN');
+INSERT INTO Contents VALUES ('7','Majesty','Jack Hayford','2','1981 New Spring','EN');
+INSERT INTO Contents VALUES ('8','Thy Loving Kindness','Hugh Mitchell','2','1956 New Spring','EN');
+INSERT INTO Contents VALUES ('9','He is my everything','Sally Ellis','3','1980 Thankyou Music','EN');
+INSERT INTO Contents VALUES ('10','Whisper a prayer','Anonymous','3',' Public Domain','EN');
+INSERT INTO Contents VALUES ('11','All over the world',' Roy Turner','3','1984 Thankyou Music','EN');
+INSERT INTO Contents VALUES ('12','I have decided','Anonymous','4','Public Domain','EN');
+INSERT INTO Contents VALUES ('13','I have enterted his gates','?','4',' ','EN');
+INSERT INTO Contents VALUES ('14','We are one in the spirit','Peter Scholte','4',' ','EN');
+INSERT INTO Contents VALUES ('15','Blessed be the tie that binds',' Johann Hans Georg Nägeli , John Fawcett','4','Public Domain','EN');
+INSERT INTO Contents VALUES ('16','Blessed Be Your Name','Matt Redman , Beth Redman','5','2002 Thankyou Music','EN');
+INSERT INTO Contents VALUES ('17','Day by Day (Blott En Dag)','Andrew L. Skoog ,Karolina Wilhelmina Sandell-Berg, Oskar Ahnfelt','5','Public Domain','EN');
+INSERT INTO Contents VALUES ('18','Hallelujah Line','Austin Miles','6',' ','EN');
+INSERT INTO Contents VALUES ('19','With Christ in the Vessel','?','6',' ','EN');
+INSERT INTO Contents VALUES ('20','Give me oil in my lamp','A. Sevison','6',' ','EN');
+INSERT INTO Contents VALUES ('21','Silver and Gold','Kirk Franklin','6',' ','EN');
+INSERT INTO Contents VALUES ('22','He is my everything','?','6',' ','EN');
+INSERT INTO Contents VALUES ('23','Trust in the Lord','Blanch Moffatt','6',' ','EN');
+INSERT INTO Contents VALUES ('24','Heaven is a wonderful place','O. A, Lambert','7','1958 Word Music, LLC','EN');
+INSERT INTO Contents VALUES ('25','Happiness is to know the Savior ','Ira Stanphill','7','1968 New Spring','EN');
+INSERT INTO Contents VALUES ('26','Jesus wants me for a Sunbeam','?','7',' ','EN');
+INSERT INTO Contents VALUES ('27','Why worry when you can pray?','Alfred B. Smith , John W. Peterson','8','1949 New Spring','EN');
+INSERT INTO Contents VALUES ('28','And Can it be','?','7',' ','EN');
+INSERT INTO Contents VALUES ('29','It is well with my soul','Audrey Assad','8',' ','EN');
+INSERT INTO Contents VALUES ('30','Count Your Blessings','Johnson Oatman, Jr.','9',' ','EN');
+INSERT INTO Contents VALUES ('31','In His Time','Diane Ball','10','1978 CCCM Music Universal Music - Brentwood Benson Publishing','EN');
+INSERT INTO Contents VALUES ('32','I Will Sing of the Mercies','James Henry Fillmore Sr. (?)','10','Public Domain','EN');
+INSERT INTO Contents VALUES ('33','He Will Carry You (There is No Problem Too Big)','?','10',' ','EN');
+INSERT INTO Contents VALUES ('34','Open Our Eyes Lord','?','10',' ','EN');
+INSERT INTO Contents VALUES ('35','The Bond of Love','Otis Skillings','11','1971 Lillenas Publishing Company','EN');
+INSERT INTO Contents VALUES ('36','Only Believe','Paul Rader','11','Public Domain','EN');
+INSERT INTO Contents VALUES ('37','Father I Adore You','Terrye Coelho','11','1972 Maranatha! Music','EN');
+INSERT INTO Contents VALUES ('38','Higher , Higher','Glen Woodward (?)','11',' ','EN');
+INSERT INTO Contents VALUES ('39','Sing Halleluljah To The Lord','Linda Stassen','12','1974 The Song Of One','EN');
+INSERT INTO Contents VALUES ('40','It is No Longer I That Liveth','Sally Ellis','12','1982 Thankyou Music','EN');
+INSERT INTO Contents VALUES ('41','My Faith Looks Upto Thee','Ray Palmer, Lowell Mason','12','Public Domain','EN');
+INSERT INTO Contents VALUES ('42','How Great thou Art (O Lord My God)','Stuart Wesley Keene Hine','13','1949 and 1953 Stuart K. Hine Trust','EN');
+INSERT INTO Contents VALUES ('43','O God ,Our Help in Ages Past','Isaac Watts , William Croft','14','Public Domain','EN');
+INSERT INTO Contents VALUES ('44','Amazing Love (You Are My King)','Chris Tomiln','14',' ','EN');
+INSERT INTO Contents VALUES ('45','In Christ Alone',' Keith Getty , Stuart Townend <br>Natalie Grant','14','2001 Thankyou Music','EN');
+INSERT INTO Contents VALUES ('46','As the Deer',' ','15',' ','EN');
+INSERT INTO Contents VALUES ('47','Hide Me in Your Holiness',' ','15',' ','EN');
+INSERT INTO Contents VALUES ('48','We Are Standing on Holy Ground',' ','15',' ','EN');
+INSERT INTO Contents VALUES ('49','Seek Ye First',' ','15',' ','EN');
+INSERT INTO Contents VALUES ('50','Glorify Thy Name',' ','16',' ','EN');
+INSERT INTO Contents VALUES ('51','Rock of Ages',' ','17',' ','EN');
+INSERT INTO Contents VALUES ('52','What a Friend we have in Jesus',' ','17',' ','EN');
+INSERT INTO Contents VALUES ('53','Jesus Paid it All',' ','18',' ','EN');
+INSERT INTO Contents VALUES ('54','How Great is Our God (The Splendour Of The King )',' Chris Tomlin','19','2004 sixsteps Music worshiptogether.com songs Wondrously Made Songs','EN');
+INSERT INTO Contents VALUES ('55','Mighty to Save',' Michael W. Smith','20',' ','EN');
+INSERT INTO Contents VALUES ('56','Before the Throne',' ','21',' ','EN');
+INSERT INTO Contents VALUES ('57','Lord reign in me',' ','22',' ','EN');
+INSERT INTO Contents VALUES ('58','Breath of God',' ','22',' ','EN');
+INSERT INTO Contents VALUES ('59','Draw Me Close',' ','22',' ','EN');
+INSERT INTO Contents VALUES ('60','Sweet Hour of Prayer',' ','23',' ','EN');
+INSERT INTO Contents VALUES ('61','10,000 Reasons (Bless The Lord)','Jonas Myrin <br> Matt Redman','24','2011 Atlas Mountain Songs, sixsteps Music, Thankyou Music ,worshiptogether.com songs','EN');
+INSERT INTO Contents VALUES ('62','Be Thou My Vision','Michael Card','24',' ','EN');
+INSERT INTO Contents VALUES ('63','The Stand','Hillsong','26',' ','EN');
+INSERT INTO Contents VALUES ('64','Lord I Need You','Matt Maher','26',' ','EN');
+INSERT INTO Contents VALUES ('65','Christ is Risen','Matt Maher','26',' ','EN');
+INSERT INTO Contents VALUES ('66','Cornerstone','Hillsong','26',' ','EN');
+INSERT INTO Contents VALUES ('67','Soul on Fire','Third Day','27',' ','EN');
+INSERT INTO Contents VALUES ('68','Desert Song','Hillsong','27',' ','EN');
+INSERT INTO Contents VALUES ('69','Hosanna','Hillsong','27',' ','EN');
+INSERT INTO Contents VALUES ('70','Lead me To The Cross','Hillsong','28',' ','EN');
+INSERT INTO Contents VALUES ('71','Magilvome , magilvome (Let us Rejoice)',' ','0',' ','Tamil');
+Update Contents Set Page_Number = NULL;
---
+++ Documents/Songs Table.sql
@@ -0,0 +1,371 @@
+Drop Table Songs;
+CREATE TABLE Songs(ID INTEGER,REFRENCE INTEGER,WORDS TEXT);
+INSERT INTO Songs VALUES ('1','1','Abba Father let me be 
+Yours and Yours alone 
+May my will forever be 
+Evermore Your own 
+Never let my heart grow cold 
+Never let me go 
+Abba Father let me be 
+Yours and Yours alone');
+INSERT INTO Songs VALUES ('2','61','<b>(Chorus)</b>
+Bless the Lord, O my soul
+O my soul
+Worship His holy name
+Sing like never before
+O my soul
+I''ll worship Your holy name
+
+The sun comes up, it''s a new day dawning
+It''s time to sing Your song again
+Whatever may pass, and whatever lies before me
+Let me be singing when the evening comes
+
+<b><i>(Chorus)</i></b>
+
+You''re rich in love, and You''re slow to anger
+Your name is great, and Your heart is kind
+For all Your goodness I will keep on singing
+Ten thousand reasons for my heart to find
+
+<b><i>(Chorus)</i></b>
+
+And on that day when my strength is failing
+The end draws near and my time has come
+Still my soul will sing Your praise unending
+Ten thousand years and then forevermore
+
+<b><i>(Chorus x2)</i></b>
+
+Jesus, I''ll worship Your holy name
+Lord, I''ll worship Your holy name
+
+Sing like never before
+O my soul
+I''ll worship Your holy name
+Jesus, I''ll worship Your holy name
+I''ll worship Your holy name');
+INSERT INTO Songs VALUES ('3','11','All over the world the Spirit is moving
+All over the world as the prophet said it would be
+All over the world there''s a mighty revelation
+Of the glory of the Lord, as the waters cover the sea
+
+All over His church God''s Spirit is moving
+All over His church as the prophet said it would be
+All over His church there''s a mighty revelation
+Of the glory of the Lord, as the waters cover the sea
+
+Right here in this place the Spirit is moving
+Right here in this place as the prophet said it would be
+Right here in this place there''s a mighty revelation
+Of the glory of the Lord, as the waters cover the sea');
+INSERT INTO Songs VALUES ('4','44','
+I''m forgiven, because You were forsaken
+I''m accepted, You were condemned
+I''m alive and well, Your Spirit is within me
+Because you died and rose again
+<i>(Repeat 2x)</i>
+
+<b>Chorus</b>
+Amazing love, how can it be
+That You my king would die for me?
+Amazing love, I know it''s true
+And It''s my joy to honor You
+In all I do I honor You
+
+I''m forgiven, because You were forsaken
+I''m accepted, You were condemned
+I''m alive and well, Your Spirit is within me
+Because you died and rose again
+');
+INSERT INTO Songs VALUES ('5','28','
+And can it be that I should gain
+An int''rest in the Savior''s blood?
+Died He for me, who caused His pain?
+For me, who Him to death pursued?
+Amazing love! how can it be
+That Thou, my God, should die for me?
+
+<b>Refrain:</b>
+Amazing love! how can it be
+That Thou, my God, should die for me!
+
+''Tis mystery all! Th''Immortal dies!
+Who can explore His strange design?
+In vain the firstborn seraph tries
+To sound the depths of love divine!
+''Tis mercy all! let earth adore,
+Let angel minds inquire no more. 
+
+<b><i>[Refrain]</i></b>
+
+He left His Father''s throne above,
+So free, so infinite His grace;
+Emptied Himself of all but love,
+And bled for Adam''s helpless race;
+''Tis mercy all, immense and free;
+For, O my God, it found out me. 
+
+<b><i>[Refrain]</i></b>
+
+Long my imprisoned spirit lay
+Fast bound in sin and nature''s night;
+Thine eye diffused a quick''ning ray,
+I woke, the dungeon flamed with light;
+My chains fell off, my heart was free;
+I rose, went forth and followed Thee. 
+
+<b><i>[Refrain]</i></b>
+
+No condemnation now I dread;
+Jesus, and all in Him is mine!
+Alive in Him, my living Head,
+And clothed in righteousness divine,
+Bold I approach th''eternal throne,
+And claim the crown, through Christ my own. 
+
+<b><i>[Refrain]</i></b>
+
+');
+INSERT INTO Songs VALUES ('6','46','
+As the deer panteth for the water
+So my soul longeth after Thee.
+You alone are my heart''s desire
+And I long to worship Thee.
+
+<b><i> Refrain : </i></b>
+You alone are my strength, my shield,
+To You alone may my spirit yield.
+You alone are my heart''s desire,
+And I long to worship Thee.
+
+You''re my friend and you are my brother
+Even though You are a King.
+I love You more than any other,
+So much more than anything. <i> [Refrain]</i>
+
+I want You more than gold or silver
+Only You can satisfy.
+You alone are the real joy giver
+And the apple of my eye. <i> [Refrain]</i>
+');
+INSERT INTO Songs VALUES ('7','62','
+Be Thou my Vision, O Lord of my heart;
+Naught be all else to me, save that Thou art.
+Thou my best Thought, by day or by night,
+Waking or sleeping, Thy presence my light.
+
+Be Thou my Wisdom, and Thou my true Word;
+I ever with Thee and Thou with me, Lord;
+Thou my great Father, I Thy true son;
+Thou in me dwelling, and I with Thee one.
+
+Be Thou my battle Shield, Sword for the fight;
+Be Thou my Dignity, Thou my Delight;
+Thou my soul’s Shelter, Thou my high Tower:
+Raise Thou me heavenward, O Power of my power.
+
+Riches I heed not, nor man’s empty praise,
+Thou mine Inheritance, now and always:
+Thou and Thou only, first in my heart,
+High King of Heaven, my Treasure Thou art.
+
+High King of Heaven, my victory won,
+May I reach Heaven’s joys, O bright Heaven’s Sun!
+Heart of my own heart, whatever befall,
+Still be my Vision, O Ruler of all.
+');
+INSERT INTO Songs VALUES ('8','56','
+Before the throne of God above
+I have a strong , a pefect plea:
+A great High Priest, whose name is Love,
+Who ever lives and pleads for me.
+
+My name is graven on his hands,
+My name is written on his heart;
+I know that while in heaven he stands
+No tongue can bid me thence depart
+No tongue can bid me thence depart.
+
+When Satan tempts me to despair,
+And tells me of the guilt within,
+Upward I look, and see him there 
+Who made an end of all my sin.
+
+Because a sinless Saviour died ,
+My sinful soul is counted free;
+For God , the Just is satisfied
+To look on Him and pardon me
+To look on Him and pardon me
+
+Hallelujah! Hallelujah!  Praise the One, 
+Risen Song of God!
+');
+INSERT INTO Songs VALUES ('9','2','
+<b>Chorus:</b>
+Bind us together, Lord
+Bind us together
+With cords that cannot be broken
+Bind us together, Lord
+Bind us together
+Bind us together with love
+
+There is only one God
+There is only one King
+There is only one Body
+That is why we sing 
+
+<b><i>(Chorus)</i></b>
+
+Made for the glory of God
+Purchased by His precious Son
+Born with the right to be clean
+For Jesus the victory has won 
+
+<b><i>(Chorus)</i></b>
+
+You are the family of God
+You are the promise divine
+You are God''s chosen desire
+You are the glorious new wine 
+
+<b><i>(Chorus)</i></b>
+');
+INSERT INTO Songs VALUES ('10','16','
+Blessed be Your name in the land that is plentiful 
+Where Your streams of abundance flow 
+Blessed be Your name 
+And blessed be Your name when I''m found in the desert place
+Though I walk through the wilderness 
+Blessed be your name 
+
+Every blessing You pour out I''ll turn back to praise 
+And when the darkness closes in Lord 
+Still I will say 
+
+Blessed be the name of the Lord 
+Blessed be Your name 
+Blessed be the name of the Lord 
+Blessed be Your glorious name 
+
+Blessed be Your name when the sun''s shining down on me 
+When the world''s “all as it should be” 
+Blessed be Your name 
+And blessed be Your name on the road marked with suffering 
+Though there''s pain in the offering 
+Blessed be Your name 
+
+You give and take away 
+You give and take away 
+My heart will choose to say 
+Lord, blessed be Your name
+');
+INSERT INTO Songs VALUES ('11','15','
+Blest be the tie that binds
+Our hearts in Christian love;
+The fellowship of kindred minds
+Is like to that above.
+
+Before our Father’s throne,
+We pour our ardent prayers;
+Our fears, our hopes, our aims are one,
+Our comforts, and our cares.
+
+We share our mutual woes,
+Our mutual burdens bear;
+And often for each other flows
+The sympathizing tear.
+
+When we asunder part,
+It gives us inward pain;
+But we shall still be joined in heart,
+And hope to meet again.
+');
+INSERT INTO Songs VALUES ('12','58','
+Breathe on me breath of God
+My spirit yearns for You
+Hide me in Your fathering arms
+Fill up my longing soul
+
+Flood over me like a river my God
+Immerse me in Your living stream
+Awaken the song that You placed in my heart
+Spirit breathe on me
+Spirit breathe on me
+
+Breathe on me breath of God
+Burn bright eternal fire
+Father draw me near to Your heart
+You are my one desire
+');
+INSERT INTO Songs VALUES ('13','65','
+Let no one caught in sin remain
+Inside the lie of inward shame
+But fix our eyes upon the cross
+And run to Him who showed great love
+And bled for us
+Freely You''ve bled for us
+
+<b>Chorus:</b>
+Christ is risen from the dead
+Trampling over death by death
+Come awake, come awake
+Come and rise up from the grave
+Christ is risen from the dead
+We are one with Him again
+Come awake, come awake
+Come and rise up from the grave
+
+Beneath the weight of all our sin
+You bowed to none but heaven''s will
+No scheme of hell, no scoffer''s crown
+No burden great can hold You down
+In strength You reign
+Forever let Your church proclaim
+
+<b><i>(Chorus)</i></b>
+
+O death, where is your sting?
+O hell, where is your victory?
+O church, come stand in the light
+The glory of God has defeated the night
+
+O death, where is your sting?
+O hell, where is your victory?
+O church, come stand in the light
+Our God is not dead
+He''s alive! He''s alive!
+
+<b><i>(Chorus 2x)</i></b>
+
+Rise up from the grave
+');
+INSERT INTO Songs VALUES ('14','66','
+My hope is built on nothing less
+Than Jesus'' blood and righteousness
+I dare not trust the sweetest frame
+But wholly trust in Jesus'' Name
+
+<b>Chorus:</b>
+Christ alone
+Cornerstone 
+Weak made strong 
+In the Savior''s love 
+Through the storm 
+He is Lord 
+Lord of all 
+
+When darkness seems to hide His face
+I rest on His unchanging grace
+In every high and stormy gale
+My anchor holds within the veil
+
+<b><i>(Chorus)</i></b>
+
+When He shall come with trumpet sound
+Oh may I then in Him be found
+Dressed in His righteousness alone
+Faultless stand before the throne
+
+<b><i>(Chorus)</i></b>
+');
